fbe89d5bbe2ada798febd38a1c6cca13608f28b2
Rasmus <rasmusedu@gmail.com>
2020-10-01 13:14:35 +0200

    python why

--- Matrix-factorization/Recom/file_reader.py
+++ Matrix-factorization/Recom/file_reader.py
@@ -1,14 +1,13 @@
-import csv
-
 import pandas as pd
 import numpy as np
-from math import sqrt
+
 
 def load_data():
     movies = pd.read_csv('movies.csv', sep=',')
     ratings = pd.read_csv('ratings.csv', sep=',')
     movie_data = pd.merge(ratings, movies, on='movieId')
-    user_movie_rating = movie_data.pivot_table(index='userId', columns='title', values='rating')
+    user_movie_rating = movie_data.pivot_table(index='userId', columns='title',
+                                               values='rating')
     return user_movie_rating
 
 
@@ -27,15 +26,18 @@ def matrix_factorization(pd_matrix):
     for step in range(steps):
         for i in range(number_of_users):
             for j in range(number_of_movies):
-                if(numpy_matrix[i][j]) > 0:
+                if (numpy_matrix[i][j]) > 0:
                     print(numpy_matrix[i][j])
-                    error = numpy_matrix[i][j] - np.dot(user_features[i, :], movie_features[:, j])
+                    error = numpy_matrix[i][j] - np.dot(user_features[i, :],
+                                                        movie_features[:, j])
 
                     for k in range(features):
-                        user_features[i][k] = user_features[i][k] + alpha * (2 * error * movie_features[k][j]
-                                                                             - beta * user_features[i][k])
-                        movie_features[k][j] = movie_features[k][j] + alpha * (2 * error * user_features[i][k]
-                                                                               - beta * movie_features[k][j])
+                        user_features[i][k] = user_features[i][k] + alpha * \
+                                              (2 * error * movie_features[k][j]
+                                               - beta * user_features[i][k])
+                        movie_features[k][j] = movie_features[k][j] + alpha * (
+                                2 * error * user_features[i][k]
+                                - beta * movie_features[k][j])
 
         prediction = np.dot(user_features, movie_features)
 
@@ -43,10 +45,14 @@ def matrix_factorization(pd_matrix):
         for i in range(number_of_users):
             for j in range(number_of_movies):
                 if (numpy_matrix[i][j]) > 0:
-                    mse = mse + pow(numpy_matrix[i][j] - np.dot(user_features[i, :], movie_features[:, j]), 2)
+                    mse = mse + pow(
+                        numpy_matrix[i][j] - np.dot(user_features[i, :],
+                                                    movie_features[:, j]), 2)
 
                     for k in range(features):
-                        mse = (mse + (beta / 2) * (pow(user_features[i][k], 2) + pow(movie_features[k][j], 2)))
+                        mse = (mse + (beta / 2) * (
+                                pow(user_features[i][k], 2) +
+                                pow(movie_features[k][j], 2)))
 
         if mse < 0.001:
             break
