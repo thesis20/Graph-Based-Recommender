56bbc5149978e233988e48a7511ac1a0c549d107
andpaacoke <and8700and@gmail.com>
2020-10-07 14:56:23 +0200

    Delete binary_tree.py

--- deepwalk_implementation/binary_tree.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, data, probability=None, left=None, right=None):
-        self.data = data
-        self.probability = probability
-        self.parent = None
-        self.depth = None
-        self.left = left
-        self.right = right
